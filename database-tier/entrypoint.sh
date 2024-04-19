728384c34227c3543f45f10edbb2dc675801e7e5
abhi8shek <bhattuabhishek8@gmail.com>
2024-04-19 10:51:54 +0530

    added fix

--- database-tier/entrypoint.sh
+++ database-tier/entrypoint.sh
@@ -1,27 +1,6 @@
-#!/bin/bash
-
-# Start MariaDB
-/usr/bin/mysqld_safe --skip-networking &
-
-# Wait for MariaDB to start
-while ! mysqladmin ping -hlocalhost --silent; do
-    sleep 1
-done
-
-# Secure MariaDB installation
-mysql_secure_installation <<EOF
-y
-${MYSQL_ROOT_PASSWORD}
-${MYSQL_ROOT_PASSWORD}
-y
-y
-y
-y
-EOF
-
 # Log into MySQL client and set up the database
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<EOF
-CREATE USER 'wiki'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+mysql -u root -p"${MARIADB_ROOT_PASSWORD}" <<EOF
+CREATE USER 'wiki'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
 CREATE DATABASE wikidatabase;
 GRANT ALL PRIVILEGES ON wikidatabase.* TO 'wiki'@'localhost';
 FLUSH PRIVILEGES;
